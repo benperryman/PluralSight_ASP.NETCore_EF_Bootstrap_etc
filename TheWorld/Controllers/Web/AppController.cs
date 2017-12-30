2d6ff2e5d52ef761a2dcff09e0ee949beefd4f03
Ben <benperryman@gmail.com>
2017-12-30 21:10:13 +0000

    Bootstrap module done. EF module half way. git push

--- TheWorld/Controllers/Web/AppController.cs
+++ TheWorld/Controllers/Web/AppController.cs
@@ -1,18 +1,60 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheWorld.Models;
+using TheWorld.Services;
+using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Web
 {
     public class AppController : Controller
     {
+        private IMailService _mailService;
+        private IConfigurationRoot _config;
+        private WorldContext _context;
+
+        public AppController(IMailService mailService, IConfigurationRoot config, WorldContext context)
+        {
+            _mailService = mailService;
+            _config = config;
+            _context = context;
+        }
+
         public IActionResult Index()
+        {
+            var data = _context.Trips.ToList();
+            return View(data);
+        }
+
+        public IActionResult Contact()
         {
             return View();
         }
-        
+
+        [HttpPost]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (model.Email.Contains("aol.com")) ModelState.AddModelError("", "We don't support AOL addresses");
+
+            if (ModelState.IsValid)
+            {
+                _mailService.SendMail(_config["MailSettings:ToAddress"], model.Email, "The World", model.Message);
+
+                ModelState.Clear();
+
+                ViewBag.UserMessage = "Message Sent";
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
     }
 }
